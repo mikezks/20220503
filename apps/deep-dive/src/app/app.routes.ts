963f5e8b15462c57d5ea6ede57ce4d2699618389
mikezks <23002661+mikezks@users.noreply.github.com>
2022-05-05 16:48:32 +0200

    feat: routing preloading

--- apps/deep-dive/src/app/app.routes.ts
+++ apps/deep-dive/src/app/app.routes.ts
@@ -19,7 +19,10 @@ export const APP_ROUTES: Routes = [
     {
         path: 'flight-booking',
         loadChildren: () => import('./flight-booking/flight-booking.module')
-          .then(esm => esm.FlightBookingModule)
+          .then(esm => esm.FlightBookingModule),
+        data: {
+          preload: true
+        }
     },
     {
         path: 'about',
