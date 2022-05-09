9edab76c8721946ea70baa0256e5e7955d964c7a
mikezks <23002661+mikezks@users.noreply.github.com>
2022-05-09 17:05:58 +0200

    feat: redux selectors, nested view model

--- apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -16,7 +16,7 @@ export class FlightSearchComponent implements OnInit {
   from = 'Hamburg'; // in Germany
   to = 'Graz'; // in Austria
   urgent = false;
-  flights$: Observable<Flight[]> = this.store.select(state => state.flightBooking.flights);
+  flights$: Observable<Flight[]> = this.store.select(fromFlightBooking.selectActiveUserFlights);
 
   // "shopping basket" with selected flights
   basket: { [id: number]: boolean } = {
